0c59f190dbd22b925064ba1fe93de254aaccf50a
aGitForEveryone <f1competitie3@gmail.com>
2024-06-16 15:24:13 +0200

    Put code behind __name__==__main__ block, and added a check to see whether eye_normal.png exists before attempting to scan for that image. This allows backwards compatibility it that screenshot was not uploaded

--- script.py
+++ script.py
@@ -1,45 +1,53 @@
+from pathlib import Path
+
 import pyautogui
 import keyboard
 
 running, isHolding = False, False
 startStopKey = "f8"
-
-print("Starting WhackaMoler@1.0.0")
-print(f"{startStopKey} - Start / Stop")
-print("-" * 20)
-print("🟩 Running" if running else "🛑 Stopped")
-
 confidence_level = 0.8
 search_region_on_screen = (350, 280, 620, 570)
+important_potato_eye = Path(__file__).parent / "img" / "eye.png"
+normal_potato_eye = Path(__file__).parent / "img" / "eye_normal.png"
 
-while True:
-    if keyboard.is_pressed(startStopKey):
-        if not isHolding:
-            isHolding = True
-            print("\033[A                             \033[A")
-            print("🛑 Stopped" if running else "🟩 Running")
-            running = not running
-    else:
-        isHolding = False
 
-    if running:
-        image_found = False
-        location = None
-        try:
-            location = pyautogui.locateOnScreen(
-                image="img/eye.png", confidence=confidence_level, region=search_region_on_screen
-            )
-            image_found = True
-        except pyautogui.ImageNotFoundException:
+if __name__ == "__main__":
+    print("Starting WhackaMoler@1.0.0")
+    print(f"{startStopKey} - Start / Stop")
+    print("-" * 20)
+    print("🟩 Running" if running else "🛑 Stopped")
+
+    while True:
+        if keyboard.is_pressed(startStopKey):
+            if not isHolding:
+                isHolding = True
+                print("\033[A                             \033[A")
+                print("🛑 Stopped" if running else "🟩 Running")
+                running = not running
+        else:
+            isHolding = False
+
+        if running:
+            image_found = False
+            location = None
             try:
                 location = pyautogui.locateOnScreen(
-                    image="img/eye_normal.png",
+                    image=str(important_potato_eye),
                     confidence=confidence_level,
                     region=search_region_on_screen,
                 )
                 image_found = True
             except pyautogui.ImageNotFoundException:
-                pass
+                try:
+                    if normal_potato_eye.exists():
+                        location = pyautogui.locateOnScreen(
+                            image=str(normal_potato_eye),
+                            confidence=confidence_level,
+                            region=search_region_on_screen,
+                        )
+                        image_found = True
+                except pyautogui.ImageNotFoundException:
+                    pass
 
-        if image_found and location:
-            pyautogui.click(location)
+            if image_found and location:
+                pyautogui.click(location)
